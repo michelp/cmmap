10f79b3bfeed61efa44b1c7f1aa1c5c0a0312c7a
Michel Pelletier <pelletier.michel@gmail.com>
2013-10-14 21:21:21 -0700

    map anonymous test.

--- test.py
+++ test.py
@@ -2,11 +2,22 @@ import tempfile
 import cmmap
 
 
-def test():
+def test_file():
     fd, filename = tempfile.mkstemp()
     f = open(filename, 'w')
-    f.write('foobarbing')
+    data = 'foobarbing'
+    f.write(data)
     f.close()
     f = open(filename, 'r+')
-    m = cmmap.mmap(prot=cmmap.PROT_READ, length=10, flags=cmmap.MAP_SHARED, fd=f.fileno())
-    assert m[:] == 'foobarbing'
+    m = cmmap.mmap(prot=cmmap.PROT_READ, length=len(data), flags=cmmap.MAP_SHARED, fd=f.fileno())
+    assert m[:] == data
+
+
+def test_anonymous():
+    data = 'foobarding'
+    m = cmmap.mmap(prot=cmmap.PROT_READ|cmmap.PROT_WRITE,
+                   length=10, 
+                   flags=cmmap.MAP_ANONYMOUS|cmmap.MAP_PRIVATE)
+    m[:] = data
+    assert m[:] == data
+
