e6b98fb53e25d570c6ffe767de0c4afd7aadeea3
Michel Pelletier <pelletier.michel@gmail.com>
2013-10-14 21:34:39 -0700

    add ipc example.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import tempfile
 import cmmap
+from multiprocessing import Process
 
 
 def test_file():
@@ -13,11 +14,30 @@ def test_file():
     assert m[:] == data
 
 
-def test_anonymous():
+def test_anonymous_private():
     data = 'foobarding'
-    m = cmmap.mmap(prot=cmmap.PROT_READ|cmmap.PROT_WRITE,
-                   length=10, 
+    m = cmmap.mmap(length=10, 
+                   prot=cmmap.PROT_READ|cmmap.PROT_WRITE,
                    flags=cmmap.MAP_ANONYMOUS|cmmap.MAP_PRIVATE)
     m[:] = data
     assert m[:] == data
 
+
+def test_anonymous_shared():
+    data = 'foobarding'
+    m = cmmap.mmap(length=10, 
+                   prot=cmmap.PROT_READ|cmmap.PROT_WRITE,
+                   flags=cmmap.MAP_ANONYMOUS|cmmap.MAP_SHARED)
+    m[:] = data
+    assert m[:] == data
+    def f():
+        assert m[:] == data
+        m[0] = 'd'
+    proc = Process(target=f)
+    proc.start()
+    proc.join()
+    assert m[:] == 'doobarding'
+    
+
+    
+        
